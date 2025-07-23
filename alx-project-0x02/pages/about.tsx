6ffc3f45e359727e1d5664b3f298d5226f02618e
whizikem <wisdomikem@gmail.com>
2025-07-23 20:30:48 +0100

    Implementing Navigation and Layout

--- alx-project-0x02/pages/about.tsx
+++ alx-project-0x02/pages/about.tsx
@@ -1,8 +1,7 @@
 // About page
 import React from "react";
 import Button from "@/components/common/Button";
-import Header
-  from "@/components/layout/Header";
+import Header from "@/components/layout/Header";
 
 const About: React.FC = () => {
   return (
